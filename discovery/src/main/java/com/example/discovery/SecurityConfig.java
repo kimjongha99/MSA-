d917181a9a844a43635340f2aac457cb033ec692
kimjongha99 <jjhsun99@gmail.com>
2023-11-13 17:50:16 +0900

    .

---
+++ discovery/src/main/java/com/example/discovery/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.discovery;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+
+@EnableWebSecurity
+@Configuration
+
+public class SecurityConfig {
+
+
+}
